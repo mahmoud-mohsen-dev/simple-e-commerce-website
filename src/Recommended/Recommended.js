3add8e6a06a2e4915c44d7ae1c880dc60e4c86c2
mody120 <mahmoud.mohsen120@gmail.com>
2023-10-24 17:35:41 +0300

    adding recommended section

--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -1,5 +1,18 @@
+import './Recommended.css'
+
 function Recommended() {
-    return ;
+    return (
+        <section className="recommended-container">
+            <h2>Recommended</h2>
+            <div className="btn-container">
+                <button className="btn">All Products</button>
+                <button className="btn">Nike</button>
+                <button className="btn">Adidas</button>
+                <button className="btn">Puma</button>
+                <button className="btn">Vans</button>
+            </div>
+        </section>
+    );
 }
 
 export default Recommended;
